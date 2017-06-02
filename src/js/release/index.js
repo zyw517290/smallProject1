76fc6a9ccc527d544a845b077dd86d2377b9efd0
zyw517290 <2523929851@qq.com>
2017-06-02 08:07:54 +0800

    first

---
+++ src/js/release/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by matrix on 2017/5/26.
+ */
+require('../../css/lib/reset.css');
+require('../../css/page/style-one.css');
+require('../../css/page/style-two.css');
+require('../../css/page/style-three.css');
+require('../../css/page/style-four.css');
+require('../lib/jquery-2.1.1.min');
+require('../lib/vue.min');
+require('../module/utils');
+require('../page/render-one');
+require('../page/render-two');
+require('../page/render-three');
+require('../page/render-four');
